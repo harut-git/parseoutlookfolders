de7646e7dbaf6c987614df094fceb012e4f870a7
harut <harut.ghubasaryan@gmail.com>
2018-11-02 18:04:57 +0400

    Build added

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from cx_Freeze import setup, Executable
 setup(name="ParseInbox",
       version="0.1",
       description="",
-      executables=[Executable("AppParseOutlookFolders.py")], requires=['gevent'])
+      executables=[Executable("AppParseOutlookFolders.py")])
